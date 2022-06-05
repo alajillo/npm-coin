de1791eb81fc144ed4bffb4388b3651bc1e913ba
yunsangjun <alajillo2021@gmail.com>
2022-06-06 00:07:34 +0900

    test : vercel env 테스트 콘솔 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import SuggestionList from '@components/SuggestionList';
 import PackageDetail from '@components/PackageDetail';
 import Slot from '@components/Slot';
 export default function Home() {
+    console.log('env test : ', process.env.VERCEL_URL);
     return (
         <>
             <SuggestionList />
